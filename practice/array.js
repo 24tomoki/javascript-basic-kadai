5e5c12348a431a06f0f474b8a575bd941a01746c
24tomoki <69114994+24tomoki@users.noreply.github.com>
2025-04-24 21:29:10 +0900

    配列処理実装

---
+++ practice/array.js
@@ -0,0 +1,6 @@
+const userNames = ['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
+console.log(userNames);
+userNames[5] = '侍五郎';
+console.log(userNames);
+console.log(userNames[3]);
+console.log(userNames.length);
