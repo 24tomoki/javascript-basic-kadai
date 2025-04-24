d0d464b9459b9e6ead0865e1fa7e59bf78d9ef77
24tomoki <69114994+24tomoki@users.noreply.github.com>
2025-04-24 22:09:07 +0900

    関数勉強

---
+++ practice/object.js
@@ -0,0 +1,6 @@
+const personalData = { name: 'sao', age: 24, gender: '女性' };
+console.log(personalData);
+personalData.age = 18;
+personalData.address = '博多区';
+console.log(personalData);
+console.log(personalData.gender);
