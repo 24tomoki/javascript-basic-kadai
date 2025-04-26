ea041c350787ff69c3ce55b7f9e1d179eb154c43
24tomoki <69114994+24tomoki@users.noreply.github.com>
2025-04-26 11:17:07 +0900

    変数のスコープ処理

--- practice/function.js
+++ practice/function.js
@@ -12,3 +12,28 @@ const sayGoodEvening = () => {
 sayGoodMorning();
 
 sayGoodEvening();
+
+const calculate = (price) => {
+  console.log(price + 500 + '円');
+}
+
+calculate(1200);
+
+const addTwoArgument = (price, shippingfree) => {
+  console.log(price + shippingfree + '円');
+}
+
+addTwoArgument(1200, 500);
+
+const double = (num) => {
+  return num * 2;
+}
+console.log(double(20));
+
+const userVariable = () => {
+  const userName = '侍太郎';
+  console.log(userName);
+}
+
+// userVariable();
+console.log(userName);
