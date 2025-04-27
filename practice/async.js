551609dd9b5db96f8f590a3ac14e42dc0467203c
24tomoki <69114994+24tomoki@users.noreply.github.com>
2025-04-27 20:01:16 +0900

    非同期処理

---
+++ practice/async.js
@@ -0,0 +1,14 @@
+console.log('処理1');
+console.log('処理2');
+console.log('処理3');
+//ここで10秒くらいかかる処理がある
+console.log('処理5');
+console.log('処理6');
+
+
+setTimeout(() => {
+  console.log('処理4(\\非同期処理\')');
+}, 2000);
+
+console.log('処理5');
+console.log('処理6');
